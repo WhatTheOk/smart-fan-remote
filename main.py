895f506c126a7ebb3be53e2038c994205518487f
WhatTheOk <98926807+WhatTheOk@users.noreply.github.com>
2023-02-27 21:29:25 -0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+def on_button_pressed_a():
+    if menu == 0:
+        fanData[0] = (fanData[0] + 1) % 5
+        basic.show_number(fanData[0])
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    if menu == 0:
+        menu = 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_button_pressed_ab():
+    if menu == 0:
+        temp = "" + str(fanData[0]) + ("" + str(fanData[1]))
+        for i in range(2, 6):
+            if fanData[i] < 10:
+                temp += "0"
+                temp += "" + ("" + ("" + str(fanData[i])))
+            else:
+                temp += "" + ("" + ("" + str(fanData[i])))
+        radio.send_string(temp)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_received_string(receivedString):
+    basic.clear_screen()
+    basic.show_string(receivedString)
+radio.on_received_string(on_received_string)
+
 radio.set_group(1)
 radio.set_transmit_power(7)
-fanData = [0, 5, 2, 30, 20, 30]
-
-
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+fanData = [0, 5, 2, 30, 5, 25]
+menu = 0
+basic.show_number(fanData[0])
--- main.ts
+++ main.ts
@@ -1,6 +1,40 @@
-radio.setGroup(1)
-radio.setTransmitPower(7)
-let fanData = [0, 5, 2, 30, 20, 30]
-basic.forever(function on_forever() {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    if (menu == 0) {
+        fanData[0] = (fanData[0] + 1) % 5
+        basic.showNumber(fanData[0])
+    }
     
 })
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let menu: number;
+    if (menu == 0) {
+        menu = 1
+    }
+    
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let temp: string;
+    if (menu == 0) {
+        temp = "" + ("" + fanData[0]) + ("" + ("" + fanData[1]))
+        for (let i = 2; i < 6; i++) {
+            if (fanData[i] < 10) {
+                temp += "0"
+                temp += "" + ("" + ("" + ("" + fanData[i])))
+            } else {
+                temp += "" + ("" + ("" + ("" + fanData[i])))
+            }
+            
+        }
+        radio.sendString(temp)
+    }
+    
+})
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
+radio.setTransmitPower(7)
+let fanData = [0, 5, 2, 30, 5, 25]
+let menu = 0
+basic.showNumber(fanData[0])
